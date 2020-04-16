41324235433de0e51ebb7a2611894af047ad78fa
duzenz <duzenz@gmail.com>
2020-04-16 23:02:55 +0300

    Create bwt.py

---
+++ bwt.py
@@ -0,0 +1,50 @@
+from operator import itemgetter
+
+def bwt(s):
+    """Apply Burrows-Wheeler transform to input string."""
+    assert "\002" not in s and "\003" not in s, "Input string cannot contain STX and ETX characters"
+    s = "\002" + s + "\003"  # Add start and end of text marker
+    table = sorted(s[i:] + s[:i] for i in range(len(s)))  # Table of rotations of string
+    last_column = [row[-1:] for row in table]  # Last characters of each row
+    return "".join(last_column)  # Convert list of characters into string
+ 
+ 
+def ibwt(r):
+    """Apply inverse Burrows-Wheeler transform."""
+    table = [""] * len(r)  # Make empty table
+    for i in range(len(r)):
+        table = sorted(r[i] + table[i] for i in range(len(r)))  # Add a column of r
+    s = [row for row in table if row.endswith("\003")][0]  # Find the correct row (ending in ETX)
+    return s.rstrip("\003").strip("\002")  # Get rid of start and end markers
+'''
+def bw_transform(s):
+    n = len(s)
+    m = sorted([s[i:n]+s[0:i] for i in range(n)])
+    I = m.index(s)
+    L = ''.join([q[-1] for q in m])
+    return (I, L)
+
+def bw_restore(I, L):
+    n = len(L)
+    X = sorted([(i, x) for i, x in enumerate(L)], key=itemgetter(1))
+
+    T = [None for i in range(n)]
+    for i, y in enumerate(X):
+        j, _ = y
+        T[j] = i
+
+    Tx = [I]
+    for i in range(1, n):
+        Tx.append(T[Tx[i-1]])
+
+    S = [L[i] for i in Tx]
+    S.reverse()
+    return ''.join(S)
+'''
+
+a = bwt("SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES");
+print(a)
+#print(ibwt(a))
+b# = bw_transform("SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES")
+#print(b)
+#print(bw_restore(6, 'zfarreo'))
