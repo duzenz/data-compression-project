9d0ef90e4ac029bafa0ea279bd44168c46deead8
duzenz <duzenz@gmail.com>
2020-04-25 00:06:41 +0300

    arithmetic coding

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''
+"""
 for byte_string in b'DABDDB DABDDBBDDBA ABRACADABRA TOBEORNOTTOBEORTOBEORNOT'.split():
     print(byte_string)
     encrypted, power, frequency = arithmetic_coding(byte_string)
@@ -8,4 +8,21 @@ for byte_string in b'DABDDB DABDDBBDDBA ABRACADABRA TOBEORNOTTOBEORTOBEORNOT'.sp
 
     if byte_string != decrypted:
         raise Exception("\tHowever that is incorrect!")
+"""
+
+'''
+encode = move_to_front_code('aaafhhimst')
+
+print(encode)
+
+decode = move_to_front_decode(encode)
+
+print(decode)
+
+'''
+
+'''
+a = burrows_wheeler_transform("SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES")
+print(a)
+print(inverse_burrows_wheeler_transform(a))
 '''
